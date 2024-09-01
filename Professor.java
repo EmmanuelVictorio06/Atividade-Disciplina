94d57b68a42e09d3f19fb15806bf016fceec08c3
Emmanuel Victorio <94030733+EmmanuelVictorio06@users.noreply.github.com>
2024-09-01 15:32:33 -0300

    Create Professor.java

---
+++ Professor.java
@@ -0,0 +1,38 @@
+public class Professor {
+    private String nome;
+    private String codigo;
+
+    // Construtor
+    public Professor(String nome, String codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nome='" + nome + '\'' +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
+}
+
+
+
